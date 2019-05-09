c6ef630b815163b63169f874e1d07aca56de0241
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-05-08 22:55:37 -0400

    game over view added

--- client/src/pages/BigBoard/BoardGameOver.js
+++ client/src/pages/BigBoard/BoardGameOver.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {HorizontalBar} from "react-chartjs-2";
 
 class BoardGameOver extends Component {
 
@@ -7,7 +8,55 @@ class BoardGameOver extends Component {
         qTotal: 15,
         qMultiple: 11,
         qTF: 1,
-        qOpen: 3
+        qOpen: 3,
+        answerData: {
+            labels: [
+                'I am Smarticus',
+                'The Quizzard of Oz',
+                'Team Sewer Cougar',
+                'The Decepticons',
+                '#AlternativeFacts',
+                'Taking Care of Quizness',
+                'Multiple Scoregasms',
+                'Rebel Scum',
+            ],
+            datasets: [
+                {
+                    data: [
+                        12,
+                        8,
+                        15,
+                        4,
+                        10,
+                        14,
+                        6,
+                        13
+                    ],
+                    backgroundColor: [
+                        "#ed4634",
+                        "#ed4634",
+                        "#34edaf",
+                        "#ed4634",
+                        "#ed4634",
+                        "#ed4634",
+                        "#ed4634",
+                        "#ed4634"
+                    ]
+                }
+            ],
+            options: {
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 0,
+                            max: 15,
+                        }      
+                    }]
+                }
+            }
+        }
     }
 
     render() {
@@ -15,10 +64,12 @@ class BoardGameOver extends Component {
         return(
             <div>
 
-                <h1>Half Time Stats!!</h1>
+                <h1>Winner: Team Sewer Cougar !!!</h1>
 
-                
-
+                <HorizontalBar
+                    data= {this.state.answerData} 
+                    options={this.state.answerData.options}
+                />
 
             </div>
         )
