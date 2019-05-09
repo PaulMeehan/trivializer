19589f6f724f0b9e2b0b855a404fe00b4ba40c6f
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-05-08 22:50:40 -0400

    halftime view added

--- client/src/pages/BigBoard/BoardHalfTime.js
+++ client/src/pages/BigBoard/BoardHalfTime.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {HorizontalBar} from "react-chartjs-2";
 
 class BoardHalfTime extends Component {
 
@@ -7,7 +8,55 @@ class BoardHalfTime extends Component {
         qTotal: 15,
         qMultiple: 11,
         qTF: 1,
-        qOpen: 3
+        qOpen: 3,
+        answerData: {
+            labels: [
+                'I am Smarticus',
+                'The Quizzard of Oz',
+                'Team Sewer Cougar',
+                'The Decepticons',
+                '#AlternativeFacts',
+                'Taking Care of Quizness',
+                'Multiple Scoregasms',
+                'Rebel Scum',
+            ],
+            datasets: [
+                {
+                    data: [
+                        6,
+                        4,
+                        8,
+                        2,
+                        5,
+                        8,
+                        3,
+                        7
+                    ],
+                    backgroundColor: [
+                        "#ed4634",
+                        "#ed4634",
+                        "#34edaf",
+                        "#ed4634",
+                        "#ed4634",
+                        "#34edaf",
+                        "#ed4634",
+                        "#ed4634"
+                    ]
+                }
+            ],
+            options: {
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 0,
+                            max: 15,
+                        }      
+                    }]
+                }
+            }
+        }
     }
 
     render() {
@@ -17,8 +66,10 @@ class BoardHalfTime extends Component {
 
                 <h1>Half Time Stats!!</h1>
 
-                
-
+                <HorizontalBar
+                    data= {this.state.answerData} 
+                    options={this.state.answerData.options}
+                />
 
             </div>
         )
