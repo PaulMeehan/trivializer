ef7a70e8b27910ed86d4bd7c751dd57a971ff01b
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-05-09 16:36:56 -0400

    more work on the admin setup + live game screens

--- client/src/pages/GameMaster/LiveGame.js
+++ client/src/pages/GameMaster/LiveGame.js
@@ -1,32 +1,123 @@
 import React, {useState, useEffect} from 'react';
 import './Admin.css';
 import {Pie} from "react-chartjs-2";
+import AdminLiveQDiv from "../../components/Admin/AdminLiveQDiv";
 
-let baseTimerData = {
-    labels: [
-        'Time Remaining',
+let preQuestions = [
+    [
+        "According to a Beatles song, who kept her face in a jar by the door?",
+        "Eleanor Rigby",
+        "Loretta Martin",
+        "Molly Jones",
+        "Lady Madonna",
+        "A"
     ],
-    datasets: [
-        {
-            data: [
-                27,
-                33
-            ],
-            backgroundColor: [
-                "#34edaf",
-                "#ed4634"
-            ]
-        }
+    [
+        "What is the stage name of English female rapper Mathangi Arulpragasam, who is known for the song &quot;Paper Planes&quot;?",
+        "K.I.A.",
+        "C.I.A.",
+        "M.I.A.",
+        "A.I.A.",
+        "C"
     ],
-    options: {
-        responsive: true
-    }
-}
+    [
+        "Which one of these Rammstein songs has two official music videos?",
+        "Du Hast",
+        "Benzin",
+        "Mein Teil",
+        "Du Riechst So Gut",
+        "D"
+    ],
+    [
+        "Which rock band released the album &quot;The Bends&quot; in March 1995?",
+        "Nirvana",
+        "Radiohead",
+        "Lemonheads",
+        "U2",
+        "B"
+    ],
+    [
+        "Which band recorded the album &quot;Parallel Lines&quot;?",
+        "The Police",
+        "Coldplay",
+        "Paramore",
+        "Blondie",
+        "D"
+    ],
+    [
+        "Which of these aliases has NOT been used by electronic musician Aphex Twin?",
+        "Burial",
+        "Caustic Window",
+        "Bradley Strider",
+        "GAK",
+        "A"
+    ]
+]
+
+
 
 const GameMasterLiveGame = () => {
 
-    const [questions, setQuestions] = useState([]);
+    let baseTimerData = {
+        labels: [
+            'Time Remaining',
+        ],
+        datasets: [
+            {
+                data: [
+                    180, // what's left
+                    0 // what's elapsed
+                ],
+                backgroundColor: [
+                    "#34edaf",
+                    "#ed4634"
+                ]
+            }
+        ],
+        options: {
+            responsive: true
+        }
+    }
+
+    const [currentQNumber,setCurrentQNumber] = useState(0);
+    const [questions, setQuestions] = useState(preQuestions);
     const [timerData, setTimerData] = useState(baseTimerData);
+
+    const decrementTimer = () => {
+        console.log("decrement run");
+        if (timerData.datasets[0].data[0] > 0) {
+            let currentTime = [timerData.datasets[0].data[0],timerData.datasets[0].data[1]];
+            currentTime = [(currentTime[0] - 1),(currentTime[1] + 1)]
+            setTimerData(
+                {
+                    datasets: [
+                        {
+                            data: currentTime,
+                            backgroundColor: [
+                                "#34edaf",
+                                "#ed4634"
+                            ]
+                        }
+                    ],
+                    options: {
+                        responsive: true
+                    }
+                }
+            )
+        }
+    }
+
+    const timerControl = () => {
+        const timerInterval = setInterval(decrementTimer,1000);
+    }
+    
+
+    const launchQuestion = (qNum) => {
+        // clearInterval(timerInterval);
+        setCurrentQNumber(qNum);
+        setTimerData(baseTimerData);
+        timerControl();
+    }
 
     return(
 
@@ -34,7 +125,7 @@ const GameMasterLiveGame = () => {
 
             <div className="row">
             
-                <div class="col-md-12">
+                <div className="col-md-12">
                     <h1>Your Game is Live!</h1>
                 </div>
 
@@ -44,63 +135,21 @@ const GameMasterLiveGame = () => {
             
                 <div className="col-md-12">
                     <div className="container">
-                        <div className="row border mt-3 p-3 deadQuestion">
-                            <div className="col-md-2">
-                                <h1>1.</h1>
-                            </div>
-                            <div className="col-md-4">
-                                <h4>This is the text of the question</h4>
-                            </div>
-                            <div className="col-md-4">
-                                <p>A. Answer Text</p>
-                                <p>B. Answer Text</p>
-                                <p>C. Answer Text</p>
-                                <p>D. Answer Text</p>
-                            </div>
-                            <div className="col-md-2">
-
-                            </div>
-                        </div>
-                        <div className="row border mt-3 p-3">
-                            <div className="col-md-1">
-                                <h1>2.</h1>
-                            </div>
-                            <div className="col-md-3">
-                                <h4>This is the text of the question</h4>
-                            </div>
-                            <div className="col-md-3">
-                                <p>A. Answer Text</p>
-                                <p>B. Answer Text</p>
-                                <p>C. Answer Text</p>
-                                <p>D. Answer Text</p>
-                            </div>
-                            <div className="col-md-2">
-                                <button className="btn-success btn">Launch Question</button>
-                                <button className="btn-danger btn mt-4">Show Answer</button>
-                            </div>
-                            <div className="col-md-3">
-                                <Pie 
-                                    data= {timerData}
-                                />
-                            </div>
-                        </div>
-                        <div className="row border mt-3 p-3">
-                            <div className="col-md-2">
-                                <h1>3.</h1>
-                            </div>
-                            <div className="col-md-4">
-                                <h4>This is the text of the question</h4>
-                            </div>
-                            <div className="col-md-4">
-                                <p>A. Answer Text</p>
-                                <p>B. Answer Text</p>
-                                <p>C. Answer Text</p>
-                                <p>D. Answer Text</p>
-                            </div>
-                            <div className="col-md-2">
-
-                            </div>
-                        </div>
+                        {questions.map( (item, index) => (
+                            <AdminLiveQDiv
+                                realQNumber = {index}
+                                qNumber = {index + 1}
+                                key = {item[0]}
+                                qText = {item[0]}
+                                a1 = {item[1]}
+                                a2 = {item[2]}
+                                a3 = {item[3]}
+                                a4 = {item[4]}
+                                timerData = {timerData}
+                                currentQNumber = {currentQNumber}
+                                launchQuestion = {launchQuestion}
+                            />
+                        ))}
                     </div>
                 </div>
 
