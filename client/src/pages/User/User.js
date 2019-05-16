e31d67b2c3ddc41556c1e441b793fdea973f88e8
PaulM <pmeehan456@gmail.com>
2019-05-16 19:19:26 -0400

    build

--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 // import gameAPI from '../../utils/gameAPI';
@@ -7,46 +7,46 @@ import { Link } from 'react-router-dom';
 let initialIncrement = 0;
 // let runChecker = setInterval(check,1000);
 
-    class User extends React.Component{
+class User extends React.Component {
 
-        state = {
-            
-            show: true,
-            questionIndex: 0,
-            curChoice: "",
-            questions: [
-                {
-                    question: "According to a Beatles song, who kept her face in a jar by the door?", 
-                    answer:  "Eleanor Rigby",
-                    choices: [
-                        "Eleanor Rigby",
-                        "Loretta Martin",
-                        "Molly Jones",
-                        "Lady Madonna"
-                    ]
-                }
-            ]
-        }
+    state = {
 
-        
-        
-        
-
-
-        handleChoiceClick = (e) => {
-            console.log(e.target.value)
-            this.setState({ curChoice: e.target.value }) 
-        }
-
-        handleSubmit = (e) => {
-            console.log(e.target.value)
-            this.setState({ show: !this.state.show })
-            if(this.state.questions[this.state.questionIndex].answer === this.state.curChoice){
-                alert("Correct")
-            }else{
-                alert("Wrong")
+        show: true,
+        questionIndex: 0,
+        curChoice: "",
+        questions: [
+            {
+                question: "According to a Beatles song, who kept her face in a jar by the door?",
+                answer: "Eleanor Rigby",
+                choices: [
+                    "Eleanor Rigby",
+                    "Loretta Martin",
+                    "Molly Jones",
+                    "Lady Madonna"
+                ]
             }
+        ]
+    }
+
+
+
+
+
+
+    handleChoiceClick = (e) => {
+        console.log(e.target.value)
+        this.setState({ curChoice: e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        console.log(e.target.value)
+        this.setState({ show: !this.state.show })
+        if (this.state.questions[this.state.questionIndex].answer === this.state.curChoice) {
+            alert("Correct")
+        } else {
+            alert("Wrong")
         }
+    }
     // let preQuestions = [
     //     [
     //         "According to a Beatles song, who kept her face in a jar by the door?",
@@ -97,8 +97,8 @@ let initialIncrement = 0;
     //         "A"
     //     ]
     // ]
-    
-    
+
+
     // check = () => {
     //     initialIncrement++;
     //     if (initialIncrement > 9) {
@@ -114,10 +114,10 @@ let initialIncrement = 0;
     //     } else {
     //         createLogLine();
     //     }
-        
+
     // }
-    
-    
+
+
 
     getQuestions = () => { // pull questions from DB
         // /api/questions
@@ -140,62 +140,62 @@ let initialIncrement = 0;
     //     getQuestions();
     // },[]);
 
-    
-render(){
-console.log(this.state);
-    var preQuestions = [
-        {
-            question: "According to a Beatles song, who kept her face in a jar by the door?", 
-            answer:  "Eleanor Rigby",
-            choices: [
-                "Eleanor Rigby",
-                "Loretta Martin",
-                "Molly Jones",
-                "Lady Madonna"
-            ]
-        }
-    ]
 
-    // const [questions, setQuestions] = useState(preQuestions);
-    // const [currentQNumber,setCurrentQNumber] = useState(); 
-    // const [isActive,setIsActive] = useState();
-    // // const [timerData, setTimerData] = useState(baseTimerData); // not doing anything at the moment
-    // const [choice, setChoice] = useState();
-    // console.log(choice, questions, setQuestions);
+    render() {
+        console.log(this.state);
+        var preQuestions = [
+            {
+                question: "According to a Beatles song, who kept her face in a jar by the door?",
+                answer: "Eleanor Rigby",
+                choices: [
+                    "Eleanor Rigby",
+                    "Loretta Martin",
+                    "Molly Jones",
+                    "Lady Madonna"
+                ]
+            }
+        ]
+
+        // const [questions, setQuestions] = useState(preQuestions);
+        // const [currentQNumber,setCurrentQNumber] = useState(); 
+        // const [isActive,setIsActive] = useState();
+        // // const [timerData, setTimerData] = useState(baseTimerData); // not doing anything at the moment
+        // const [choice, setChoice] = useState();
+        // console.log(choice, questions, setQuestions);
 
 
-    return(
-        <div style={{ borderRadius: '5px', maxWidth: '420px', margin: 'auto'}}>
-            <nav>
-                <Link to="/" className="navbar-brand"><img style={{ width: '100%'}} src="/trivializer-logo.png" alt="" className="logo"/></Link>
-            </nav>
-            <div style={{ borderRadius: '5px', margin: 'auto', backgroundColor: '#eee', maxWidth: "420px", height: 'auto' }}>
-                 <div style={{ display: 'block', width: '25%', margin: 'auto' }}><img src="https://img.icons8.com/color/96/000000/alarm-clock.png" alt=""/>
-                </div>
-                <div>   
-                    <h1 style={{ textAlign: 'center', color: 'black'}}>Question: { this.state.questionIndex + 1 } </h1>
-                    <div style={{ padding: '10px',}}>
-                        <p style={{ textAlign: 'center', color: '#9800ff' }}>{preQuestions[0].question}</p>
-                            {this.state.show?
-                                <div style={{display: 'flex', justifyContent: 'center',  alignItems: 'center', width: '75%'}}> 
+        return (
+            <div style={{ borderRadius: '5px', maxWidth: '420px', margin: 'auto' }}>
+                <nav>
+                    <Link to="/" className="navbar-brand"><img style={{ width: '100%' }} src="/trivializer-logo.png" alt="" className="logo" /></Link>
+                </nav>
+                <div style={{ borderRadius: '5px', margin: 'auto', backgroundColor: '#eee', maxWidth: "420px", height: 'auto' }}>
+                    <div style={{ display: 'block', width: '25%', margin: 'auto' }}><img src="https://img.icons8.com/color/96/000000/alarm-clock.png" alt="" />
+                    </div>
+                    <div>
+                        <h1 style={{ textAlign: 'center', color: 'black' }}>Question: {this.state.questionIndex + 1} </h1>
+                        <div style={{ padding: '10px', }}>
+                            <p style={{ textAlign: 'center', color: '#9800ff' }}>{preQuestions[0].question}</p>
+                            {this.state.show ?
+                                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '75%' }}>
                                     <ul style={{ display: 'block', margin: 'auto', listStyleType: 'none', color: '#9800ff' }}>
-                                    
+
                                         {this.state.questions[this.state.questionIndex].choices.map((singleChoice) => {
-                                            return(<li><input type="radio" checked={ this.state.curChoice === singleChoice } value={ singleChoice } onChange={this.handleChoiceClick}></input>{singleChoice}</li>)
+                                            return (<li><input type="radio" checked={this.state.curChoice === singleChoice} value={singleChoice} onChange={this.handleChoiceClick}></input>{singleChoice}</li>)
                                         })}
 
-                                        <button style={{ borderRadius: '5px', height: '30px', width: '100%', backgroundColor: '#f9800ff'  }} onClick={this.handleSubmit}>Submit</button>
+                                        <button style={{ borderRadius: '5px', height: '30px', width: '100%', backgroundColor: '#f9800ff' }} onClick={this.handleSubmit}>Submit</button>
                                     </ul>
-                                </div> 
-                        :<p style={{ padding: '15px', fontFamily: 'karmatic arcade', textAlign: 'center', color: '#9800ff'}}>Good choice.  But, you an idiot.</p>}
+                                </div>
+                                : <p style={{ padding: '15px', fontFamily: 'karmatic arcade', textAlign: 'center', color: '#9800ff' }}>Good choice.  But, you an idiot.</p>}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <footer>            
-                <Link to="/" className="navbar-brand"><img style={{ width: '100%'}} src="/trivializer-logo.png" alt="" className="logo"/></Link>
-            </footer>
+                <footer>
+                    <Link to="/" className="navbar-brand"><img style={{ width: '100%' }} src="/trivializer-logo.png" alt="" className="logo" /></Link>
+                </footer>
 
-        </div>
+            </div>
         );
     }
 }
