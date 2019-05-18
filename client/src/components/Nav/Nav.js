a3fcfeda02c292c9fc723eb08cba05b3bcef7caf
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-05-18 19:14:18 -0400

    several layout fixes/updates

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -12,14 +12,14 @@ const Nav = (props) => {
 		greeting = (
 			<Fragment>
 				Welcome back, <strong>{props.user.firstName}</strong>
-        - <Link to="#" className="logout" onClick={props.logout}>Logout</Link>
+        &nbsp; <Link to="#" className="logout" onClick={props.logout}>[Logout]</Link>
 			</Fragment>
 		)
 	} else if (props.user.username) {
 		greeting = (
 			<Fragment>
 				Welcome back, <strong>{props.user.username} </strong>
-        - <Link to="#" className="logout" onClick={props.logout}>Logout</Link>
+        &nbsp; <Link to="#" className="logout" onClick={props.logout}>[Logout]</Link>
 			</Fragment>
 		)
   }
@@ -35,11 +35,11 @@ const Nav = (props) => {
     linkLocation = "/board-question";
     greeting = "";
   } else if (window.location.pathname.toLowerCase() === "/board-question") {
-    location = props.user.username
+    location = "by\xa0\xa0\xa0" + props.user.username
     linkLocation = "/statsboard";
     greeting = "";
-  } else if (window.location.pathname.toLowerCase() === "/live-game") {
-    location = "YOUR GAME IS LIVE!!!"
+  } else if (window.location.pathname.toLowerCase() === "/live-game" || "/admin") {
+    location = "by\xa0\xa0\xa0" + props.user.username
     linkLocation = "#";
   }
 
@@ -47,7 +47,7 @@ const Nav = (props) => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <Col size="md-9">
         <Link to="/" className="navbar-brand"><img src="/trivializer-logo.png" className="logo"/></Link>
-        <Link to={linkLocation}><h1 style={ {display: "inline-block" } } className="align-middle m-0 ml-4 p-0 pageTitle">by&nbsp;&nbsp;&nbsp;{location}</h1></Link>
+        <Link to={linkLocation}><h1 style={ {display: "inline-block" } } className="align-middle m-0 ml-4 p-0 pageTitle">{location}</h1></Link>
       </Col>
       {/* <Col size="md-7"></Col> */}
       <Col size="md-3">
--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -42,7 +42,8 @@ class LandingPage extends React.Component {
 
         <div className="row mt-5">
           <div className="col-md-12">
-            <a href="/admin" className="btn btn-large btn-block btn-success"><h1>Host a Game</h1></a>
+            <a href="/admin" className="btn btn-large btn-block btn-success"><h1 className="fiddle">Host a Game</h1></a>
+
             <h1 className="text-center m-5">- OR -</h1>
 
             <div className="input-group mb-3">
@@ -58,11 +59,12 @@ class LandingPage extends React.Component {
                 onChange={(e) => this.handleInputChange(e)}
               >
               </input>
-              <a
-                href={`/play/${this.state.game}`}
+              <Link 
+                to={`/play/${this.state.game}`}
                 className="btn btn-success btn-large"
-                type="button"
-              ><h1>Play a Game</h1></a>
+              >
+                <h1 className="fiddle">Play a Game</h1>
+              </Link>
             </div>
 
           </div>
