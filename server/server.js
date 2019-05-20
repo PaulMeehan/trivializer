c12d88b13d3e78d672c1cbac4c7ebe2cf886445d
dPaq <dpaquette2.0@gmail.com>
2019-05-20 14:40:53 -0400

    more

--- server/server.js
+++ server/server.js
@@ -35,6 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 	console.log('YOU ARE IN THE PRODUCTION ENV');
 	app.use(express.static(path.join(__dirname, '../client/build')));
 	app.get('/*', (req, res) => {
+		res.setHeader('Last-Modified', (new Date()).toUTCString())
 		res.sendFile(path.join(__dirname, '../client/build/index.html'))
 	});
 }
