de69e8b2acc2b36768cf580054075cd65747c86d
dPaq <dpaquette2.0@gmail.com>
2019-05-19 21:03:28 -0400

    fixed delete button issue on Admin.js

--- client/src/pages/GameMaster/Admin.js
+++ client/src/pages/GameMaster/Admin.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './Admin.css';
-import AdminGameDiv from "../../components/Admin/AdminGameDiv";
+// import AdminGameDiv from "../../components/Admin/AdminGameDiv";
 import gameAPI from '../../utils/gameAPI'
 import { Link } from 'react-router-dom';
 
@@ -10,40 +10,33 @@ const defaultQuestionTime = 180
 
 const GameMasterAdmin = () => {
 
-  const [questions, setQuestions] = useState([{question:"stillEmpty"}]);
+  const [questions, setQuestions] = useState([]);
   const [newQ, setNewQ] = useState();
   const [newA1, setNewA1] = useState();
   const [newA2, setNewA2] = useState();
   const [newA3, setNewA3] = useState();
   const [newA4, setNewA4] = useState();
   const [newCorrect, setNewCorrect] = useState();
-  const [questionCount, setQuestionCount] = useState("stillEmpty");
+  // const [questionCount, setQuestionCount] = useState();
 
   // console.log("questions[0].question = " + questions[0].question)
 
   useEffect(() => {
     gameAPI.getQuestions()
-      .then(function(res) {
-        console.log("res.data.game = ");
-        console.log(res.data.game);
-        if (res.data.game.length !== 0) {
-          setQuestions(res.data.game)
-          setQuestionCount(res.data.game.length -1)
-        }
-      })
+      .then(res => setQuestions(res.data.game))
       .catch(err => console.log(err))
   }, [])
 
   const deleteQuestion = id => {
-    // const tempQuestions = [...questions]
-    const tempQuestions = questions && questions.length > 0 ? [...questions] : [];
-
-    setQuestions(tempQuestions.splice(id,1))
+    console.log(id)
+    const tempQuestions = []
+    for (let i = 0; i < questions.length; i++) {
+      if (i !== id) tempQuestions.push(questions[i])
+    }
+    console.log(tempQuestions)
+    setQuestions(tempQuestions)
     gameAPI.setQuestions(tempQuestions)
-      .then(function(res) {
-        setQuestions(res.data.game)
-        setQuestionCount(res.data.game.length -1)
-      })
+      .then(res => setQuestions(res.data.game))
       .catch(err => console.log(err))
   }
 
@@ -60,51 +53,78 @@ const GameMasterAdmin = () => {
   }
 
   const addQuestion = () => {
-    console.log ("In addQuestion");
-    console.log (newCorrect);
     if (!newCorrect) return
-    // console.log(tempQuestions)
-    // const tempQuestions = [...questions]
-    let tempQuestions = questions && questions.length > 0 ? [...questions] : [];
+    let tempQuestions = questions && questions.length > 0 ? [...questions] : []
     const newQuestion = {
       question: newQ,
       choices: [newA1, newA2, newA3, newA4],
       answer: newCorrect,
       time: defaultQuestionTime
     }
-    console.log(tempQuestions, newQuestion)
-    if (questions[0].question === "stillEmpty") { // if the "stillEmpty" placehold is still present, we want to overwrite the array with only this new question
-      tempQuestions = [newQuestion] 
-    } else { // otherwise it's business as usual
-      tempQuestions.push(newQuestion)
-    }
+    tempQuestions.push(newQuestion)
     gameAPI.setQuestions(tempQuestions)
-      .then(function(res) {
+      .then((res) => {
         setQuestions(res.data.game)
-        setQuestionCount(res.data.game.length -1)
-        setNewQ('');
-        setNewA1('');
-        setNewA2('');
-        setNewA3('');
-        setNewA4('');
-        setNewCorrect('');
         document.getElementById("addQuestionForm").reset();
       })
       .catch(err => console.log(err))
   }
 
-  console.log({questions})
+  /*
+    Component
+  */
+  const AdminGameDiv = (q, i) => {
+    return (
+      <div className="row border mb-3 p-3" key={i*100}>
+        <div className="col-md-1" key={i*100 + 1}>
+          <h1 key={i*100 + 2}>{i + 1}.</h1>
+        </div>
+        <div className="col-md-4" key={i*100 + 3}>
+          <h4 key={i*100 + 4}>{q.question}</h4>
+        </div>
+        <div className="col-md-4" key={i*100 + 5}>
+          <p className={ (q.answer === "A") ? "correctAnswer" : "" } key={i*100 + 6}>A. {q.choices[0]}</p>
+          <p className={ (q.answer === "B") ? "correctAnswer" : "" } key={i*100 + 7}>B. {q.choices[1]}</p>
+          <p className={ (q.answer === "C") ? "correctAnswer" : "" } key={i*100 + 8}>C. {q.choices[2]}</p>
+          <p className={ (q.answer === "D") ? "correctAnswer" : "" } key={i*100 + 9}>D. {q.choices[3]}</p>
+        </div>
+        <div className="col-md-3" key={i*100 + 10}>
+          <button
+            key={i*100 + 11}
+            onClick={() => deleteQuestion(i)}
+            className="btn btn-danger"
+          >Delete Question</button>
+        </div>
+      </div>
+    )
+  }
+
+  const DrawAdminGameDiv = last => {
+    last = last || false
+    let block = [], q
+    if (last === true) {
+      const qNum = Math.max(questions.length - 1, 0)
+      q = questions[qNum]
+      if (q) block.push(AdminGameDiv(q,qNum))
+      return block
+    }
+    if (questions.length >= 0) {
+      for (let i = 0; i < questions.length; i++) {
+        q = questions[i]
+        block.push(AdminGameDiv(q,i))
+      }
+    }
+    return block
+  }
 
   return(
     <div className="container">
       {/* header div */}
-
       <div className="row">
         <div className="col-md-12">
           <Link to="/live-game" className="btn btn-success btn-large btn-block mb-4"><h2>Start Your Game</h2></Link>
         </div>
       </div>
-
       <div className="row">
         <div className="col-md-4 p-0">
           <div className="header">
@@ -151,98 +171,30 @@ const GameMasterAdmin = () => {
         <div className="col-md-1"></div>
         <div className="col-md-7 p-0 m-0">
 
+          {/* Most recent question */}
           <div className="header">
             <h2>Last Question Added:</h2>
           </div>
           <div className="border p-3 m-0 mb-5">
               <div className="container">
-
-                {console.log("questionCount = " + questionCount)}
-
-                { 
-                // TERNARY (DETERMINING IF questionCount HAS BEEN UPDATED)
-                (questionCount === "stillEmpty" || questions[0].question === "stillEmpty") ? <h4>No current questions</h4> : 
-                
-                // <h4>Trying to show the last question</h4>
-                // console.log("trying to spit out <AdminGameDiv>")
-                <AdminGameDiv
-                  realQNumber = {questionCount}
-                  qNumber = {questionCount + 1}
-                  key = {questions[questionCount].question}
-                  qText = {questions[questionCount].question}
-                  a1 = {questions[questionCount].choices[0]}
-                  a2 = {questions[questionCount].choices[1]}
-                  a3 = {questions[questionCount].choices[2]}
-                  a4 = {questions[questionCount].choices[3]}
-                  correct = {questions[questionCount].answer}
-                  deleteQuestion = {deleteQuestion}
-                />
-                
-                }
-                
-
-
-
-                {/* <div className="row border mt-3 p-3">
-                  <div className="col-md-1">
-                    <h1>{questionCount}.</h1>
-                  </div>
-                  <div className="col-md-4">
-                    <h4>{ (questions[questionCount]) ? questions[questionCount].question : ""}</h4>
-                  </div>
-                  {/* <div className="col-md-4">
-                    <p className={ (questions[questions.length].answer == "A") ? "correctAnswer" : "" }>A. {questions[questions.length].choices[0]}</p>
-                    <p className={ (questions[questions.length].answer == "B") ? "correctAnswer" : "" }>B. {questions[questions.length].choices[1]}</p>
-                    <p className={ (questions[questions.length].answer == "C") ? "correctAnswer" : "" }>C. {questions[questions.length].choices[2]}</p>
-                    <p className={ (questions[questions.length].answer == "D") ? "correctAnswer" : "" }>D. {questions[questions.length].choices[3]}</p>
-                  </div> */}
-                  {/* <div className="col-md-3"> */}
-                    {/* <button
-                      onClick={() => props.deleteQuestion(props.realQNumber)}
-                      className="btn btn-light"
-                    >Delete Question</button> */}
-                  {/* </div>
-                </div> */}
+                {DrawAdminGameDiv(true)}
               </div>
           </div> {/* close border wrapper div */}
 
+          {/* All saved questions */}
           <div className="header">
             <h2>All Saved Questions:</h2>
           </div>
           <div className="border p-3 m-0">
               <div className="container">
-              { (questions[0].question !== "stillEmpty") ?
-                (
-                  questions.map( (question, i) => {
-                    return (
-                      <AdminGameDiv
-                        realQNumber = {i}
-                        qNumber = {i + 1}
-                        key = {question.question}
-                        qText = {question.question}
-                        a1 = {question.choices[0]}
-                        a2 = {question.choices[1]}
-                        a3 = {question.choices[2]}
-                        a4 = {question.choices[3]}
-                        correct = {question.answer}
-                        deleteQuestion = {deleteQuestion}
-                      />
-                    )
-                  })
-                ) : (
-                  <h4>No current questions</h4>
-                ) }
+                <DrawAdminGameDiv />
               </div>
           </div> {/* close border wrapper div */}
 
         </div> {/* close md-7 col */}
       </div>
-      {/* new uestion div */}
-      
-      {/* saved questions div */}
       <div className="row border mt-4">
         <div className="col-md-12">
-          
         </div>
       </div>
     </div>
